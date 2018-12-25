25cb6b1b5c3dd5b9ff7b658021da5704932a31e5
alexha <alexh.90@live.com>
2018-12-25 03:04:50 -0800

    create basic table layout w/ flex

--- frontend/src/components/Lobby.js
+++ frontend/src/components/Lobby.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
+
+import Table from "../components/Table";
+
+import { CONTENT_CONTAINER } from "../constants/UI/Sizing"
 //==============================================================//
+
+const DesktopContainer = styled.section`
+  width: ${CONTENT_CONTAINER}px;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  margin: 10vh auto;
+`;
+
+const Row = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+  order: ${props => props.order};
+  margin: ${props => props.middle ? "80px 0" : "0"};
+`;
+
+const EmptyDivider = styled.div`
+  user-select: none;
+  min-width: ${CONTENT_CONTAINER/4}px;
+  flex-basis: 100%;
+`;
 
 class Lobby extends Component {
   constructor() {
@@ -12,9 +38,23 @@ class Lobby extends Component {
 
   render() {
     return (
-      <div>
-        in lobby
-      </div>
+      <DesktopContainer>
+        <Row order={1}>
+          <EmptyDivider />
+          <Table number={1}/>
+          <EmptyDivider />
+        </Row>
+        <Row order={2} middle={true}>
+          <Table number={2}/>
+          <EmptyDivider />
+          <Table number={3}/>
+        </Row>
+        <Row order={3}>
+          <EmptyDivider />
+          <Table number={4}/>
+          <EmptyDivider />
+        </Row>
+      </DesktopContainer>
     )
   }
 }
---
+++ frontend/src/constants/UI/Sizing.js
@@ -0,0 +1,1 @@
+export const CONTENT_CONTAINER: number = 1024;
